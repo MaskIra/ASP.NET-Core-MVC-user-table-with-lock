5119144a81be7914f21637c0b481c01fc219527c
Irina Maskevich <55229323+MaskIra@users.noreply.github.com>
2022-04-12 16:51:05 +0300

    Add files via upload

---
+++ WebForUsers/WebForUsers/ViewModels/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebForUsers.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "email not specified")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "name not specified")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "password not specified")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "password entered incorrectly")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ WebForUsers/WebForUsers/wwwroot/js/checkboxs.js
@@ -0,0 +1,14 @@
+﻿$(window).load(function () {
+    $("#checkAll").change(function () {
+        $("input:checkbox:visible").prop('checked', $(this).prop("checked"));
+    });
+    $('input:checkbox').not(this).change(function () {
+        var chbs = $('input:checkbox').not(this);
+        if (chbs.length == chbs.filter(":checked").length) {
+            $("#checkAll").prop('checked', this.checked);
+        }
+        if (chbs.length == chbs.filter(":checked").length + 1 && this.checked) {
+            $("#checkAll").prop('checked', this.checked);
+        }
+    });
+});
